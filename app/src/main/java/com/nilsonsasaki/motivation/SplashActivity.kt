71533c68880b0aa99ff19043c42d093df6062696
nilsonsasaki <nilsonsasaki@gmail.com>
2022-01-12 10:23:20 -0300

    added force light mode

--- app/src/main/java/com/nilsonsasaki/motivation/SplashActivity.kt
+++ app/src/main/java/com/nilsonsasaki/motivation/SplashActivity.kt
@@ -1,13 +1,15 @@
 package com.nilsonsasaki.motivation
 
-import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatDelegate
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
 
         if(supportActionBar!=null){
             supportActionBar!!.hide()
