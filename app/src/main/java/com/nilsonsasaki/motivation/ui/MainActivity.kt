a55b2daa3065f5fd4dbbf8b5270bcb888bece8bd
nilsonsasaki <nilsonsasaki@gmail.com>
2022-01-13 17:56:12 -0300

    added loaded name to textView

--- app/src/main/java/com/nilsonsasaki/motivation/ui/MainActivity.kt
+++ app/src/main/java/com/nilsonsasaki/motivation/ui/MainActivity.kt
@@ -2,11 +2,24 @@ package com.nilsonsasaki.motivation.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.nilsonsasaki.motivation.R
+import com.nilsonsasaki.motivation.databinding.ActivityMainBinding
+import com.nilsonsasaki.motivation.infra.MotivationConstants
+import com.nilsonsasaki.motivation.infra.SecurityPreferences
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var _binding: ActivityMainBinding
+    private lateinit var mSecurityPreferences: SecurityPreferences
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(_binding.root)
+
+        mSecurityPreferences = SecurityPreferences(this)
+
+        _binding.tvHello.text = mSecurityPreferences.getString(MotivationConstants.KEY.PERSON_NAME)
+
     }
 }
